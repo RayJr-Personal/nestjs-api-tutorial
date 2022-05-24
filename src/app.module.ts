97849c9ca66cc72a72a6502c2b58d3e9ee7e8971
MageBruh <raymondstevejr@yahoo.com>
2022-05-24 21:15:55 +1200

    Added config (env)

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,19 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 // Decorator: adds metadata to the current class/function; adds more properties to the class
 // Module: decorator, annotates AppModule, organizes your app
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+    UserModule,
+    BookmarkModule,
+    PrismaModule,
+  ],
 })
 export class AppModule {}
