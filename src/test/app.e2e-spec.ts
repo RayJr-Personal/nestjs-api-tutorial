0d57e091baca4699cde0926ef3fa3eed054f2599
MageBruh <raymondstevejr@yahoo.com>
2022-05-29 23:57:40 +1200

    Moved test files

--- src/test/app.e2e-spec.ts
+++
@@ -1,9 +0,0 @@
-// Using Pactum and Jest
-describe('App e2e', () => {
-  beforeAll(async () => {
-    const moduleRef = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-  });
-  it.todo('should pass');
-});
